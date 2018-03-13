eb7cb6ba43c445025655dc2ace57897ab1c26065
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-03-13 16:26:48 +0100

    exo 4

--- ex04.js
+++ ex04.js
@@ -13,7 +13,7 @@
     et nous attendons comme résultat 1;
 */
 
-isEven(6);
+//isEven(6);
 
 /*      Test 2
     Appel à la fonction "isEven",
@@ -21,6 +21,24 @@ isEven(6);
     et nous attendons comme résultat 0;
 */
 
-isEven(5);
-
+//isEven(5);
+/*
 //  écrire votre code sous ce commentaire
+*/
+
+// Je crée ma fonction, et lui donne deux conditions, avec un calcul sur le modulo si pair ou impair
+function isEven (nb){
+    if (nb % 2 === 0){
+        console.log(nb + " est un chiffre pair")
+        return 0;
+
+        }
+    else{
+        console.log(`${nb} est un chiffre impair`)
+        return 1;
+        }
+}
+
+// Je donne les valeurs à ma fonction
+isEven(6);
+isEven(5);
