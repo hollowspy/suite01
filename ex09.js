f8b7c6ad322a0711e8ccf3ad3cda7c373a938984
Julien NIEDZWIECKI <julien.niedzwiecki@neuf.fr>
2018-03-13 17:34:25 +0100

    exo 9 avec annotations

--- ex09.js
+++ ex09.js
@@ -22,6 +22,10 @@ boostedEvenAddition([4, 5, 6]);
 boostedEvenAddition([1, 3, 4, 5, 4, 6, 9, 8, 11, 10, 11, 12]);
 
 //  écrire votre code sous ce commentaire
+// Création de la fonction. Ajout d'une variable total pair.
+// Boucle sur le tableau. Si la valeur de l'index du tableau est pair (module égal à 0)
+// alors tu rajoutes totalPair à lui même.
+// Attention, ne pas faire le console log dans la boucle, sinon, c'est moche ^^
  function boostedEvenAddition (array){
      let totalPair = 0
      for (var i = 0; i < array.length; i++) {
